22c568381c86612267f20a7938179b5ffb5f5a96
ymtdir <ymtdir0407@gmail.com>
2025-08-03 13:34:21 +0900

    chore: Viteのプロキシ設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
