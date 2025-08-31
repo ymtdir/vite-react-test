7885b236bf091005d37a742e4c921b1a481d0082
ymtdir <ymtdir0407@gmail.com>
2025-08-31 18:19:38 +0900

    feat: サイドバーのモックアップを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { createGuestRoutes, createProtectedRoutes } from "@/routes/index";
-
-const router = createBrowserRouter([
-  ...createGuestRoutes(),
-  ...createProtectedRoutes(),
-]);
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+      </main>
+    </SidebarProvider>
+  );
 }
 
 export default App;
